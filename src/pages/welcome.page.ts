bc1e28f22b3b56b5cc9dcaffa11fc8d6cf52b321
dawidgil <dawid.gil@j-labs.pl>
2022-01-10 21:57:12 +0100

    add: types and method for checking if welcome page is displayed.

--- src/pages/welcome.page.ts
+++ src/pages/welcome.page.ts
@@ -2,15 +2,19 @@ import { WelcomeSelector } from '../models/welcome-selector.enum';
 import { BaseAbstract } from './base.abstract';
 
 export class WelcomePage extends BaseAbstract {
-  async selectMetamask() {
+  async isWelcomePage(): Promise<boolean> {
+    return Boolean(await page.$('app-welcome'));
+  }
+
+  async selectMetamask(): Promise<void> {
     await page.click(this.getSelector(WelcomeSelector.MetamaskButton));
   }
 
-  async selectAzure() {
+  async selectAzure(): Promise<void> {
     await page.click(this.getSelector(WelcomeSelector.AzureButton));
   }
 
-  async selectWalletConnect() {
+  async selectWalletConnect(): Promise<void> {
     await page.click(this.getSelector(WelcomeSelector.WalletConnect));
   }
 
