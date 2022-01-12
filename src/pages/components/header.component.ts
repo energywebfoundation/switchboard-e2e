7c1c62adc4354322382f8db52a1a1c03069aaa44
dawidgil <dawid.gil@j-labs.pl>
2022-01-12 11:46:40 +0100

    feat: extend header component with new methods

--- src/pages/components/header.component.ts
+++ src/pages/components/header.component.ts
@@ -1,3 +1,27 @@
-export class HeaderComponent {
+import { BaseAbstract } from '../base.abstract';
 
+export class HeaderComponent extends BaseAbstract {
+  private readonly ENROLMENT_SELECTOR = 'header-enrolment';
+  private readonly ASSETS_SELECTOR = 'header-assets';
+  private readonly GOVERNANCE_SELECTOR = 'header-governance';
+
+  async areNavigationButtonsVisible(): Promise<boolean> {
+    return Boolean(page.$(this.getSelector(this.GOVERNANCE_SELECTOR)));
+  }
+
+  async isExperimentalEnabled(): Promise<boolean> {
+    return Boolean(page.$(this.getSelector(this.ASSETS_SELECTOR)));
+  }
+
+  async navigateToAssets(): Promise<void> {
+    await page.click(this.getSelector(this.ASSETS_SELECTOR));
+  }
+
+  async navigateToGovernance(): Promise<void> {
+    await page.click(this.getSelector(this.GOVERNANCE_SELECTOR));
+  }
+
+  async navigateToEnrolments(): Promise<void> {
+    await page.click(this.getSelector(this.ENROLMENT_SELECTOR));
+  }
 }
