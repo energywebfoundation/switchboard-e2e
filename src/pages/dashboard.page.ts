c28223c1ac6bcdaaf13b92eac8ca5f5f169f15c3
dawidgil <dawid.gil@j-labs.pl>
2022-01-12 11:04:14 +0100

    feat: create dashboard page with dashboard component and header component

---
+++ src/pages/dashboard.page.ts
@@ -0,0 +1,7 @@
+import { HeaderComponent } from './components/header.component';
+import { DashboardComponent } from './components/dashboard.component';
+
+export class DashboardPage {
+  header: HeaderComponent = new HeaderComponent();
+  dashboard: DashboardComponent = new DashboardComponent();
+}
