3977f0e7a89e1aeb88b84ebd9d82eb8c0688b1aa
dawidgil <dawid.gil@j-labs.pl>
2022-01-12 11:53:47 +0100

    feat: extend dashboard component with new methods

--- src/pages/components/dashboard.component.ts
+++ src/pages/components/dashboard.component.ts
@@ -1,3 +1,19 @@
-export class DashboardComponent {
+import { BaseAbstract } from '../base.abstract';
 
+export class DashboardComponent extends BaseAbstract {
+  private readonly ENROLMENT_SELECTOR = 'Enrolments';
+  private readonly ASSETS_SELECTOR = 'Assets';
+  private readonly GOVERNANCE_SELECTOR = 'Governance';
+
+  async getAssetsCard(): Promise<void> {
+    await page.$(this.getSelector(this.ASSETS_SELECTOR));
+  }
+
+  async getGovernanceCard(): Promise<void> {
+    await page.$(this.getSelector(this.GOVERNANCE_SELECTOR));
+  }
+
+  async getEnrolmentsCard(): Promise<void> {
+    await page.$(this.getSelector(this.ENROLMENT_SELECTOR));
+  }
 }
