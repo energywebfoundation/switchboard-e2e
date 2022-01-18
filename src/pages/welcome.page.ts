2702e84410475c114e5d2a565c1922cde076b089
dawidgil <dawid.gil@j-labs.pl>
2022-01-18 16:58:44 +0100

    fix: test for login only with localstorage data.

--- src/pages/welcome.page.ts
+++ src/pages/welcome.page.ts
@@ -36,6 +36,7 @@ export class WelcomePage extends BaseAbstract {
     // set localstorage to the page
     await page.evaluate(() => {
       localStorage.setItem('ProviderType', 'MetaMask');
+      localStorage.setItem('isEthSigner', 'true');
       localStorage.setItem('PublicKey', '0230379d9ecb9d8cc7a41beff5ec8b7382db7f38df0b9d3188ffce57ac0557c755');
     });
   }
