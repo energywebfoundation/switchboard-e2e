0fc9992fb14192ef5c3998e24fc6f4d3398f4c17
dawidgil <dawid.gil@j-labs.pl>
2022-09-09 11:18:44 +0200

    feat: method for filling localstorage for reinitialization

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
 export const CONFIG  = {
-  page: 'https://switchboard-dev.energyweb.org'
+  page: 'https://switchboard-dev.energyweb.org',
+  publicKey: '0230379d9ecb9d8cc7a41beff5ec8b7382db7f38df0b9d3188ffce57ac0557c755'
 }
--- src/pages/welcome.page.ts
+++ src/pages/welcome.page.ts
@@ -1,5 +1,6 @@
 import { WelcomeSelector } from '../models/welcome-selector.enum';
 import { BaseAbstract } from './base.abstract';
+import { CONFIG } from '../config';
 
 export class WelcomePage extends BaseAbstract {
   async isWelcomePage(): Promise<boolean> {
@@ -36,10 +37,14 @@ export class WelcomePage extends BaseAbstract {
     await page.bringToFront();
 
     // set localstorage to the page
+    await this.fillLocalstorageWithDataForReinitialization();
+  }
+
+  async fillLocalstorageWithDataForReinitialization() {
     await page.evaluate(() => {
       localStorage.setItem('ProviderType', 'MetaMask');
       localStorage.setItem('isEthSigner', 'true');
-      localStorage.setItem('PublicKey', '0230379d9ecb9d8cc7a41beff5ec8b7382db7f38df0b9d3188ffce57ac0557c755');
+      localStorage.setItem('PublicKey', CONFIG.publicKey);
     });
   }
 
